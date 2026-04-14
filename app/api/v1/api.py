f3ad18cfd476f42d6e1c49d6da6b32887959975e
Rishi <kukadiyarishi895@gmail.com>
2026-04-14 08:49:36 +0000

    transformer model removed due to not enough resources

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -2,10 +2,10 @@ from fastapi import APIRouter
 from app.api.v1.endpoints import audit
 from app.api.v1.endpoints import struct_audit_api
 from app.api.v1.endpoints import graph_audit
-from app.api.v1.endpoints import model_audit
+
 
 api_router = APIRouter()
 api_router.include_router(audit.router, prefix="/audit", tags=["audit"])
 api_router.include_router(struct_audit_api.router, prefix="/struct-audit", tags=["struct-audit"])
 api_router.include_router(graph_audit.router , prefix="/graph" , tags=["graphs-audit"])
-api_router.include_router(model_audit.router , prefix="/model" , tags=["model-audit"])
+
